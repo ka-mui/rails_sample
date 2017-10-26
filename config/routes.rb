a086454939b0ebc514f0a817cc9ab87510a03870
k_horikawa <k_horikawa@horikwa-kamui.local>
2017-10-27 01:43:13 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
 
+  get 'static_pages/contact'
+
+  get 'foo/bar'
+
+  get 'foo/baz'
+
+  get 'static_pages/about'
+
   get 'static_pages/help'
 
-  root 'application#hello'
+  root 'static_pages#home'
 end
