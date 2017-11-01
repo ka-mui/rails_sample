fd8f1b3e731fa1f980dcdb8ae321efe68074ab13
k_horikawa <k_horikawa@horikwa-kamui.local>
2017-11-01 17:41:21 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/contact'
-
-  get 'foo/bar'
-
-  get 'foo/baz'
-
-  get 'static_pages/about'
-
-  get 'static_pages/help'
+  get 'users/new'
 
   root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help', as: 'helf'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
